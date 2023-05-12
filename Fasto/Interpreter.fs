2f85152928ff40f93ad3c93b1c604f6eea1bc086
Tobias Legenden <tobiaslegenden@gmail.com>
2023-05-13 01:20:16 +0200

    fixed OR to match assignment text

--- Fasto/Interpreter.fs
+++ Fasto/Interpreter.fs
@@ -169,13 +169,10 @@ let rec evalExp (e : UntypedExp, vtab : VarTable, ftab : FunTable) : Value =
   | Or (e1, e2, pos) ->
         let res1 = evalExp(e1, vtab, ftab)
         let res2 = evalExp(e2, vtab, ftab)
-        match (res1, res2) with
-          | (BoolVal true, BoolVal false) -> BoolVal true
-          | (BoolVal false, BoolVal true) -> BoolVal true
-          | (BoolVal true, BoolVal true) -> BoolVal true
-          | (BoolVal false, BoolVal false) -> BoolVal false
-          | (BoolVal _, _) -> reportWrongType "right operand of ||" Int res2 (expPos e2)
-          | (_, _) -> reportWrongType "left operand of ||" Int res1 (expPos e1)
+        match (res1) with
+          | (BoolVal true) -> BoolVal true
+          | (BoolVal false) -> res2
+          | (_) -> reportWrongType "left operand of ||" Int res1 (expPos e1)
   | Not(_, _) ->
         failwith "Unimplemented interpretation of negate"
   | Negate(_, _) ->
