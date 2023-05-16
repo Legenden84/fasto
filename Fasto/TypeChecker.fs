7a92ae94bc35fc9b9b10c6a489121e5805cdc9e1
Boris Pahhota <bopah91@hotmail.com>
2023-05-16 15:08:49 +0200

    task2 some stuff

--- Fasto/TypeChecker.fs
+++ Fasto/TypeChecker.fs
@@ -311,8 +311,28 @@ and checkExp  (ftab : FunTable)
             - `arr` should be of type `[ta]`
             - the result of filter should have type `[tb]`
     *)
-    | Filter (_, _, _, _) ->
-        failwith "Unimplemented type check of filter"
+    | Filter (f, arr_exp, _, pos) ->
+        let (arr_type, arr_exp_dec) = checkExp ftab vtab arr_exp
+        let elem_type =
+            match arr_type with
+              | Array t -> t
+              | _ -> reportTypeWrongKind "second argument of filter" "array" arr_type pos
+
+        let (f', f_res_type, f_arg_type) =
+            match checkFunArg ftab vtab pos f with
+              | (f', res, [a1]) -> (f', res, a1)
+              | (_, res, args) ->
+                   reportArityWrong "first argument of filter" 1 (args,res) pos
+
+        if f_res_type <> Bool then
+          reportError ("function-return type is not bool") pos
+
+        if elem_type <> f_arg_type then
+          reportTypesDifferent "function-argument and array-element types in filter"
+                               f_arg_type elem_type pos
+
+        (Array f_res_type, Filter (f', arr_exp_dec, elem_type, pos))
+
 
     (* TODO project task 2: `scan(f, ne, arr)`
         Hint: Implementation is very similar to `reduce(f, ne, arr)`.
